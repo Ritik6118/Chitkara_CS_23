4b3131f6fb2094cb76a5fbf84bc9ab5c9c7e501c
Ritik Kumar <70974819+Ritik6118@users.noreply.github.com>
2023-10-20 12:30:12 +0530

    Update good_subarray.java

--- src/Question_Bank/good_subarray.java
+++ src/Question_Bank/good_subarray.java
@@ -15,7 +15,7 @@ public class good_subarray {
 		// TODO Auto-generated method stub
 		int c=0;
 		for(int i=0;i<arr.length;i++) {
-			for(int j=i+1;j<arr.length;j++) {
+			for(int j=i;j<arr.length;j++) {
 				if(isgood(arr,i,j,d)) {
 					c++;
 				}
